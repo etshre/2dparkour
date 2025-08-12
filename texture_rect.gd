ccc9bdb3ce36498037617ad2bab1e2a812e0c128
etshre <estherr.w54@gmail.com>
2025-08-12 14:02:33 +0100

    making boxes and drag and drop code

---
+++ texture_rect.gd
@@ -0,0 +1,33 @@
+extends TextureRect
+
+
+func _get_drag_data(_at_position):
+	
+	var preview_texture = TextureRect.new()
+	
+	preview_texture.texture = texture
+	preview_texture.expand_mode = 1
+	preview_texture.size = Vector2(40,40)
+	
+	var preview = Control.new()
+	preview.add_child(preview_texture)
+	
+	set_drag_preview(preview)
+	texture = null
+	
+	return preview_texture.texture
+	
+func _can_drop_data(_pos, data):
+	return data is Texture2D
+	
+func _drop_data(_pos, data):
+	texture = null
+	speed = 0
+	direction = 0
+	
+var speed = 50
+var direction = 1 #1 is to the right
+
+
+func _process(delta):
+	position.x+= speed * direction * delta
